3e4a69f83aec2f674e6491f15d593791f4eca983
Eamon Burns (Laptop WSL) <eamon.r.burns@gmail.com>
2024-05-20 12:48:25 -0700

    Initial commit

---
+++ simple_prompt.py
@@ -0,0 +1,14 @@
+from bark import SAMPLE_RATE, generate_audio, preload_models
+from scipy.io import wavfile
+
+# Download and load all models
+preload_models()
+
+# Generate audio from prompt
+text_prompt = """
+    Hello, I am robot.
+"""
+audio_array = generate_audio(text_prompt)
+
+# Write to a .wav file
+wavfile.write("bark_generation.wav", SAMPLE_RATE, audio_array)
